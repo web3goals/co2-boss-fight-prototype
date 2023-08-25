daa12400e6c74f5bbcf202e1ac35b8ae4511adac
kiv1n <aloha@kiv1n.ru>
2023-08-25 19:28:22 +0300

    Add empty page to create boss

---
+++ app/pages/bosses/create.tsx
@@ -0,0 +1,8 @@
+import Layout from "@/components/layout";
+
+/**
+ * Page to create a boss.
+ */
+export default function BossCreate() {
+  return <Layout maxWidth="sm">...</Layout>;
+}
--- app/pages/bosses/index.tsx
+++ app/pages/bosses/index.tsx
@@ -1,8 +1,28 @@
 import Layout from "@/components/layout";
+import { LargeLoadingButton } from "@/components/styled";
+import { Box, Typography } from "@mui/material";
+import Link from "next/link";
 
 /**
  * Page with bosses.
  */
 export default function Bosses() {
-  return <Layout maxWidth="sm">...</Layout>;
+  return (
+    <Layout maxWidth="sm">
+      <Typography variant="h4" fontWeight={700} textAlign="center">
+        ⚔️ Choose a boss to fight
+      </Typography>
+      <Typography textAlign="center" mt={1}>
+        or create one that emit CO2 and damage the environment in your city
+      </Typography>
+      <Box display="flex" flexDirection="column" alignItems="center" mt={2}>
+        <Link href="/bosses/create">
+          <LargeLoadingButton variant="contained">
+            Create Boss
+          </LargeLoadingButton>
+        </Link>
+      </Box>
+      <Typography>...</Typography>
+    </Layout>
+  );
 }
