d6f5fd4f8589245be0905f15f89269b7d409ee94
kiv1n <aloha@kiv1n.ru>
2023-08-26 18:02:56 +0300

    Add form to track user geoposition

--- app/pages/bosses/[id].tsx
+++ app/pages/bosses/[id].tsx
@@ -1,11 +1,17 @@
 import Layout from "@/components/layout";
-import { FullWidthSkeleton, LargeLoadingButton } from "@/components/styled";
+import {
+  CardBox,
+  FullWidthSkeleton,
+  LargeLoadingButton,
+} from "@/components/styled";
 import { BOSS_IMAGES } from "@/constants/bosses";
 import { bossContractAbi } from "@/contracts/abi/bossContract";
+import useError from "@/hooks/useError";
 import useUriDataLoader from "@/hooks/useUriDataLoader";
 import { theme } from "@/theme";
 import { BossUriData } from "@/types";
 import { chainToSupportedChainConfig } from "@/utils/chains";
+import { calculateDistance } from "@/utils/geo";
 import {
   Avatar,
   Box,
@@ -14,9 +20,11 @@ import {
   SxProps,
   Typography,
 } from "@mui/material";
+import { grey } from "@mui/material/colors";
 import { Stack } from "@mui/system";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { useContractRead, useNetwork } from "wagmi";
 
 /**
@@ -26,6 +34,7 @@ export default function Boss() {
   const router = useRouter();
   const { id } = router.query;
   const { chain } = useNetwork();
+  const isAlive = true; // TODO: Define real data
 
   /**
    * Define uri and uri data
@@ -34,7 +43,8 @@ export default function Boss() {
     address: chainToSupportedChainConfig(chain).contracts.boss,
     abi: bossContractAbi,
     functionName: "tokenURI",
-    args: [BigInt(id as string)],
+    args: [id ? BigInt(id as string) : BigInt(0)],
+    enabled: id !== undefined,
   });
   const { data: uriData } = useUriDataLoader<BossUriData>(uri);
 
@@ -43,6 +53,7 @@ export default function Boss() {
       {uriData ? (
         <>
           <BossDescription id={Number(id)} uriData={uriData} />
+          {isAlive && <BossTrackMovement sx={{ mt: 4 }} />}
         </>
       ) : (
         <FullWidthSkeleton />
@@ -111,7 +122,6 @@ function BossDescription(props: {
           <CircularProgress
             variant="determinate"
             value={(currentHealth / props.uriData.health) * 100}
-            disableShrink
             sx={{
               color: "#000000",
               animationDuration: "550ms",
@@ -142,5 +152,119 @@ function BossDescription(props: {
         </LargeLoadingButton>
       </Link>
     </Box>
+  );
+}
+
+function BossTrackMovement(props: { sx?: SxProps }) {
+  const router = useRouter();
+  const { dev } = router.query;
+  const { handleError } = useError();
+  const [isTracking, setIsTracking] = useState(false);
+  const [tracker, setTracker] = useState<NodeJS.Timeout | undefined>();
+  const [currentPosition, setCurrentPosition] = useState<
+    GeolocationPosition | undefined
+  >();
+  const [previousPosition, setPreviousPosition] = useState<
+    GeolocationPosition | undefined
+  >();
+  const [distance, setDistance] = useState(0.0);
+
+  /**
+   * Enabled or disable tracker for geo position
+   */
+  useEffect(() => {
+    if (isTracking) {
+      const tracker = setInterval(() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setCurrentPosition(position);
+          },
+          (error) => {
+            handleError(new Error(error.message), true);
+          }
+        );
+      }, 3_000);
+      setTracker(tracker);
+    } else {
+      // TODO: Save distance using CO2 storage
+      clearTimeout(tracker);
+      setTracker(undefined);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isTracking]);
+
+  /**
+   * Update distance using position
+   */
+  useEffect(() => {
+    if (currentPosition && previousPosition) {
+      if (dev) {
+        setDistance(distance + 0.42);
+      } else {
+        setDistance(
+          distance +
+            calculateDistance(
+              currentPosition.coords.latitude,
+              currentPosition.coords.longitude,
+              previousPosition.coords.latitude,
+              previousPosition.coords.longitude
+            )
+        );
+      }
+    }
+    setPreviousPosition(currentPosition);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPosition]);
+
+  return (
+    <CardBox
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      sx={{ ...props.sx }}
+    >
+      <Typography variant="h6" textAlign="center">
+        <strong>🛰️ Track your movement</strong> using{" "}
+        <strong>alternative transportation</strong> to defeat the boss
+      </Typography>
+      {!isTracking && (
+        <Typography variant="body2" textAlign="center" mt={1}>
+          1 km reduces CO2 emissions by 192g
+        </Typography>
+      )}
+      {isTracking ? (
+        <Stack
+          direction="row"
+          spacing={2}
+          alignItems="center"
+          mt={2}
+          sx={{ background: grey[200], pr: 4, borderRadius: "78px" }}
+        >
+          <LargeLoadingButton
+            variant="contained"
+            sx={{ mt: 2 }}
+            onClick={() => setIsTracking(false)}
+          >
+            Stop
+          </LargeLoadingButton>
+          <Stack alignItems="center">
+            <Typography variant="body2" fontWeight={700}>
+              {distance.toFixed(2)} km / {(distance * 192).toFixed(2)} g CO2
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              You’ve tracked
+            </Typography>
+          </Stack>
+        </Stack>
+      ) : (
+        <LargeLoadingButton
+          variant="contained"
+          sx={{ mt: 2 }}
+          onClick={() => setIsTracking(true)}
+        >
+          Start
+        </LargeLoadingButton>
+      )}
+    </CardBox>
   );
 }
